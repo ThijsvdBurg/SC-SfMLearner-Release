fad207e382df874f2aad059b51b726ac5873cfdf
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-01 15:17:03 +0100

    new scripts for comparing different pretrained weights and image size

--- scripts/husky/train_husky5.sh
+++ scripts/husky/train_husky5.sh
@@ -1,18 +1,22 @@
-TRAIN_SET=datasets/tu_delft_husky/zed/
+ TRAIN_SET=datasets/tu_delft_husky/zed/
+POSE_NET=checkpoints/r18_husky/exp_pose_model_best.pth.tar
+DISP_NET=checkpoints/r18_husky/dispnet_model_best.pth.tar
 
 python train.py $TRAIN_SET \
 --folder-type sequence \
 --resnet-layers 18 \
 --num-scales 1 \
--s0.1 -c0.5 --epochs 50 \
+-s0.1 -c0.5 --epochs 2 \
 --with-ssim 1 \
 --with-mask 1 \
 --with-auto-mask 1 \
 --with-pretrain 1 \
 --log-output \
 --dataset nyu \
---name r18_husky \
+--name r18_husky_832_256_imnet_pretrain \
 --img-width 832 \
---img-height 256
+--img-height 256 \
+--pretrained-pose $POSE_NET \
+--pretrained-disp $DISP_NET
 
-# -b16 -s0.1 -c0.5 --epoch-size 0 --epochs 50 \
+# -b16 -s0.1 -c0.5 --epoch-size 0 --epochs 2 \
---
+++ scripts/husky/train_husky7_832_256_pretrained-res18kitt-recnyu19_pose_3ep.sh
@@ -0,0 +1,22 @@
+TRAIN_SET=datasets/tu_delft_husky/zed/
+POSE_NET=checkpoints/resnet18_depth_256/exp_pose_model_best.pth.tar
+DISP_NET=checkpoints/rectified_nyu_r18/dispnet_model_best.pth.tar
+
+python train.py $TRAIN_SET \
+--folder-type sequence \
+--resnet-layers 18 \
+--num-scales 1 \
+-s0.1 -c0.5 --epochs 1 \
+--with-ssim 1 \
+--with-mask 1 \
+--with-auto-mask 1 \
+--with-pretrain 1 \
+--log-output \
+--dataset nyu \
+--name r18_husky_832_256_pretrained_nyu_disp_kitti_pose_3ep \
+--img-width 832 \
+--img-height 256 \
+--pretrained-pose $POSE_NET \
+--pretrained-disp $DISP_NET
+
+# -b16 -s0.1 -c0.5 --epoch-size 0 --epochs 50 \
