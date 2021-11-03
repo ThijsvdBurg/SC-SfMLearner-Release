4d42a2776b5e33771daa9fba4f1055692d7fa1a1
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-03 17:06:49 +0100

    add and change inference and train scripts for husky

---
+++ scripts/husky/train_husky_640_360_pretrained-nyu_disp_and_kitt256_pose_singles_5ep.sh
@@ -0,0 +1,23 @@
+TRAIN_SET=datasets/tu_delft_husky/zed/
+RESNET_FOLDER=checkpoints/r18_husky_640_360_pretrained_nyu_disp_and_kitt256_pose_singles/4ep
+POSE_NET=$RESNET_FOLDER/exp_pose_model_best.pth.tar
+DISP_NET=$RESNET_FOLDER/dispnet_model_best.pth.tar
+
+python train.py $TRAIN_SET \
+--folder-type sequence \
+--resnet-layers 18 \
+--num-scales 1 \
+-b4 -s0.1 -c0.5 --epochs 1 \
+--with-ssim 1 \
+--with-mask 1 \
+--with-auto-mask 1 \
+--with-pretrain 1 \
+--log-output \
+--dataset nyu \
+--name r18_husky_640_360_pretrained_kitti_256_disp_and_pose_singles \
+--img-width 640 \
+--img-height 360 \
+--pretrained-pose $POSE_NET \
+--pretrained-disp $DISP_NET
+
+# -b16 -s0.1 -c0.5 --epoch-size 0 --epochs 50 \
