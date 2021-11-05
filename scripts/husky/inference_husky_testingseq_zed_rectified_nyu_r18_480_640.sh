2891f3ce4dd96ed1e98ca86fcbac26d5a8058598
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-05 15:04:20 +0100

    change scripts

--- scripts/husky/inference_husky_testingseq_zed_rectified_nyu_r18_480_640.sh
+++ scripts/husky/inference_husky_testingseq_zed_rectified_nyu_r18_480_640.sh
@@ -1,9 +1,9 @@
 
 # points to location of RGB images
-INPUT_DIR=datasets/tu_delft_husky/testingseq
+INPUT_DIR=datasets/tu_delft_husky/zed/seq007
 
 # points to directory to output resulting depth images
-OUTPUT_DIR=results/husky_testingseq_zed_rectified_nyu_r18_480_640
+OUTPUT_DIR=results/husky_testingseq_zed_rectified_nyu_r18_480_640/seq007
 
 # points to trained model
 DISPNET=checkpoints/rectified_nyu_r18/dispnet_model_best.pth.tar
