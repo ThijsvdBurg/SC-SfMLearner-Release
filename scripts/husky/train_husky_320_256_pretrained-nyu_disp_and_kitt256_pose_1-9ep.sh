6b1f81cd97bbcc131b1c60f370349c8ea0e6fc72
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-18 09:29:57 +0100

    add inference sh scripts

---
+++ scripts/husky/train_husky_320_256_pretrained-nyu_disp_and_kitt256_pose_1-9ep.sh
@@ -0,0 +1,23 @@
+TRAIN_SET=datasets/tu_delft_husky/zed/
+RESNET_FOLDER=checkpoints/nyu_disp_kitti_pose
+POSE_NET=$RESNET_FOLDER/exp_pose_model_best.pth.tar
+DISP_NET=$RESNET_FOLDER/dispnet_model_best.pth.tar
+
+python train.py $TRAIN_SET \
+--folder-type sequence \
+--resnet-layers 18 \
+--num-scales 1 \
+-b8 -s0.1 -c0.5 --epochs 9 \
+--with-ssim 1 \
+--with-mask 1 \
+--with-auto-mask 1 \
+--with-pretrain 1 \
+--log-output \
+--dataset nyu \
+--name r18_husky_320_256_pretrained_kitti_256_disp_and_pose \
+--img-width 320 \
+--img-height 256 \
+--pretrained-pose $POSE_NET \
+--pretrained-disp $DISP_NET
+
+# -b16 -s0.1 -c0.5 --epoch-size 0 --epochs 50 \
--- scripts/report_inference/report_inference_zed_r18_husky_320_256_pretrained_nyu_disp_kitti_pose_320_256_9ep.sh
+++ scripts/report_inference/report_inference_zed_r18_husky_320_256_pretrained_nyu_disp_kitti_pose_320_256_9ep.sh
@@ -1,14 +1,11 @@
 # points to location of RGB images
-
 INPUT_DIR=datasets/tu_delft_husky/report_test_seq
 
 # points to directory to output resulting depth images
-
-OUTPUT_DIR=results/20211116/husky_report_test_seq_zed_rectified_nyu_r18_640_480
+OUTPUT_DIR=results/20211116/husky_report_test_seq_r18_husky_320_256_PT_nyu_disp_kitti_pose_9ep/
 
 # points to trained model
-
-DISPNET=checkpoints/rectified_nyu_r18/dispnet_model_best.pth.tar
+DISPNET=checkpoints/r18_husky_320_256_pretrained_kitti_256_disp_and_pose/9ep/dispnet_model_best.pth.tar
 
 # actually run the inference on each image
 python3 run_inference_scale_result_to_640_480.py \
@@ -18,8 +15,8 @@ python3 run_inference_scale_result_to_640_480.py \
 --output-dir $OUTPUT_DIR \
 --output-disp \
 --output-depth \
---img-width 640 \
---img-height 480
+--img-width 320 \
+--img-height 256
 
 
 
