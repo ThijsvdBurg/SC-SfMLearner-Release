086715c4ae9ad4cc997cff07bc95f6ee5f2d54a5
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-19 11:00:55 +0100

    change scripts

---
+++ scripts/report_inference/report_inference_zed_r18_husky_320_256_pretrained_nyu_disp_kitti_pose_320_256_3ep.sh
@@ -0,0 +1,23 @@
+# points to location of RGB images
+INPUT_DIR=datasets/tu_delft_husky/report_test_seq
+
+# points to directory to output resulting depth images
+OUTPUT_DIR=results/20211119/r18_husky_320_256_PT_nyu_disp_kitti_pose_3ep/
+
+# points to trained model
+DISPNET=checkpoints/r18_husky_320_256_pretrained_nyu_disp_kitti_pose/3ep/dispnet_model_best.pth.tar
+
+# actually run the inference on each image
+python3 run_inference_scale_result_to_640_480.py \
+--pretrained $DISPNET \
+--resnet-layers 18 \
+--dataset-dir $INPUT_DIR \
+--output-dir $OUTPUT_DIR \
+--output-disp \
+--output-depth \
+--img-width 320 \
+--img-height 256
+
+
+
+# --dataset-list $LIST_DIR
