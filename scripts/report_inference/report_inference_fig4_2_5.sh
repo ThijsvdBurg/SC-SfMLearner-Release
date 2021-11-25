e5b1839ca3abbcb346ddfb3b18ea24b226eb8f61
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-25 16:51:19 +0100

    change scripts

--- scripts/report_inference/report_inference_fig4_2_5.sh
+++ scripts/report_inference/report_inference_fig4_2_5.sh
@@ -5,8 +5,11 @@ INPUT_DIR=datasets/tu_delft_husky/report_test_seq
 OUTPUT_DIR=results/report results/20211125_kitti_832_nyu_320_husky_640_and_320/
 
 DISPNET=checkpoints/r18_husky_640_480_pretrained_nyu_disp_kitti_pose/9ep/dispnet_model_best.pth.tar
+
 OUTPUT_NAME=r18_husky_640_480_pretrained_nyu_disp_kitti_pose_9ep
+
 IMW=640
+
 IMH=480
 # actually run the inference on each image
 python3 run_inference_scale_result_to_640_480.py \
@@ -14,7 +17,7 @@ python3 run_inference_scale_result_to_640_480.py \
 --resnet-layers 18 \
 --dataset-dir $INPUT_DIR \
 --outdir $OUTPUT_DIR \
---outname $OUTPUT_NAME \
 --output-depth \
 --img-width $IMW \
 --img-height $IMH
+# --outname $OUTPUT_NAME \
