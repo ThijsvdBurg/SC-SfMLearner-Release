a821aa6450932c5dbe833034ed92c66d873f955a
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-25 16:53:13 +0100

    change scripts

--- scripts/report_inference/report_inference_fig4_2_5.sh
+++ scripts/report_inference/report_inference_fig4_2_5.sh
@@ -2,7 +2,7 @@
 INPUT_DIR=datasets/tu_delft_husky/report_test_seq
 
 # points to directory to output resulting depth images
-OUTPUT_DIR=results/report results/20211125_kitti_832_nyu_320_husky_640_and_320/
+OUTPUT_DIR=results/report_results/20211125_kitti_832_nyu_320_husky_640_and_320/
 
 DISPNET=checkpoints/r18_husky_640_480_pretrained_nyu_disp_kitti_pose/9ep/dispnet_model_best.pth.tar
 
@@ -17,7 +17,7 @@ python3 run_inference_scale_result_to_640_480.py \
 --resnet-layers 18 \
 --dataset-dir $INPUT_DIR \
 --outdir $OUTPUT_DIR \
+--outname $OUTPUT_NAME \
 --output-depth \
 --img-width $IMW \
 --img-height $IMH
-# --outname $OUTPUT_NAME \
