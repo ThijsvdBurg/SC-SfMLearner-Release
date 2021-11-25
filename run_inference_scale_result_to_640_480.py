dcbef6b777512cbfa616b1cbfc1c2dd39997cd57
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-25 12:46:48 +0100

    change scripts

--- run_inference_scale_result_to_640_480.py
+++ run_inference_scale_result_to_640_480.py
@@ -107,7 +107,8 @@ def main():
             # print('min depth after t2a is: {}'.format(depth3.min().item()))
 
             # imsave(output_dir/'{}_depth_max_None{}'.format(file_name, file_ext), dept_transpose)
-            imsave(output_dir/'{}_640_480{}'.format(file_name,file_ext),cv2.resize(dept_transpose,(640,480)))
+
+            imsave(output_dir/'{}_{}{}'.format(file_name,args.output_name,file_ext),cv2.resize(dept_transpose,(640,480)))
             # print(output_dir,file_name,file_ext)
 
             # depth = (255*tensor2array(output, max_value=None, colormap='rainbow' )).astype(np.uint8)
--- scripts/report_inference/report_inference_fig4_2_4.sh
+++ scripts/report_inference/report_inference_fig4_2_4.sh
@@ -6,12 +6,16 @@ OUTPUT_DIR=results/report results/20211125/
 
 # points to trained model
 DISPNET=checkpoints/r18_husky_320_256_pretrained_kitti_256_disp_and_pose/9ep/dispnet_model_best.pth.tar
+OUTPUT_NAME=r18_husky_320_256_pretrained_kitti_256_disp_and_pose_9ep
+IMW=
+IMH=
 # actually run the inference on each image
 python3 run_inference_scale_result_to_640_480.py \
 --pretrained $DISPNET \
 --resnet-layers 18 \
 --dataset-dir $INPUT_DIR \
 --output-dir $OUTPUT_DIR \
+--output-name $OUTPUT_NAME \
 --output-depth \
 --img-width $IMW \
 --img-height $IMH
