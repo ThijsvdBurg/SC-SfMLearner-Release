65f3dd133acc70efe54688b9e12ed662c02eead1
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-25 16:07:38 +0100

    change scripts

--- scripts/report_inference/report_inference_fig4_2_4.sh
+++ scripts/report_inference/report_inference_fig4_2_4.sh
@@ -4,7 +4,6 @@ INPUT_DIR=datasets/tu_delft_husky/report_test_seq
 # points to directory to output resulting depth images
 OUTPUT_DIR=results/report results/20211125_kitti_832_nyu_320_husky_640_and_320/
 
-
 DISPNET=checkpoints/r18_husky_640_480_pretrained_nyu_disp_kitti_pose/9ep/dispnet_model_best.pth.tar
 OUTPUT_NAME=r18_husky_640_480_pretrained_nyu_disp_kitti_pose_9ep
 IMW=640
@@ -15,6 +14,7 @@ python3 run_inference_scale_result_to_640_480.py \
 --resnet-layers 18 \
 --dataset-dir $INPUT_DIR \
 --output-dir $OUTPUT_DIR \
+--output-name $OUTPUT_NAME \
 --output-depth \
 --img-width $IMW \
 --img-height $IMH
@@ -28,6 +28,7 @@ python3 run_inference_scale_result_to_640_480.py \
 --pretrained $DISPNET \
 --resnet-layers 18 \
 --dataset-dir $INPUT_DIR \
+--output-name $OUTPUT_NAME \
 --output-dir $OUTPUT_DIR \
 --output-depth \
 --img-width $IMW \
@@ -44,6 +45,7 @@ python3 run_inference_scale_result_to_640_480.py \
 --resnet-layers 18 \
 --dataset-dir $INPUT_DIR \
 --output-dir $OUTPUT_DIR \
+--output-name $OUTPUT_NAME \
 --output-depth \
 --img-width $IMW \
 --img-height $IMH
@@ -58,6 +60,7 @@ python3 run_inference_scale_result_to_640_480.py \
 --resnet-layers 18 \
 --dataset-dir $INPUT_DIR \
 --output-dir $OUTPUT_DIR \
+--output-name $OUTPUT_NAME \
 --output-depth \
 --img-width $IMW \
 --img-height $IMH
