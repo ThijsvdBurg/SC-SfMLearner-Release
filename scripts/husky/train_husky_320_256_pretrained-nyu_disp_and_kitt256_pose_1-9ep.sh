04d0d27e87b8d48978455479505bc86481193224
ThijsvdBurg <thijsvdburg95@gmail.com>
2021-11-26 10:30:33 +0100

    change scripts

--- scripts/husky/train_husky_320_256_pretrained-nyu_disp_and_kitt256_pose_1-9ep.sh
+++ scripts/husky/train_husky_320_256_pretrained-nyu_disp_and_kitt256_pose_1-9ep.sh
@@ -14,10 +14,8 @@ python train.py $TRAIN_SET \
 --with-pretrain 1 \
 --log-output \
 --dataset nyu \
---name r18_husky_320_256_pretrained_kitti_256_disp_and_pose \
+--name r18_husky_320_256_pretrained_nyu_disp_kitti_pose_9ep \
 --img-width 320 \
 --img-height 256 \
 --pretrained-pose $POSE_NET \
 --pretrained-disp $DISP_NET
-
-# -b16 -s0.1 -c0.5 --epoch-size 0 --epochs 50 \
